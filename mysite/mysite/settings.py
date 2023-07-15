9ae50d4ec787d2cfc506d1207a9ae27532cc3c0c
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2023-07-15 22:09:59 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w26avkm1ud&y&bv&pw^1v$g4ijjlk@$mle4@xo9=x%v3wqtgkd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["phyedcheck1-production.up.railway.app"]
+ALLOWED_HOSTS = ["phyed.up.railway.app"]
 
 
 # Application definition
